725a48ceeb9e0dc3552f07b56b9978ab0ca9c89a
AlexanderHolistic <alexander.holike@gmail.com>
2024-07-23 13:23:09 +0200

    second change test

--- Program.cs
+++ Program.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("First Change!");
+        Console.WriteLine("Second Change!");
     }
 }
