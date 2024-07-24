16a11ad88683177ad4cb27ad3ce092417457fcd5
Alexander Holike <alexander.holike@gmail.com>
2024-07-24 12:06:57 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,12 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Second Change!");
-		Console.WriteLine("My Own Change(Paule1511)");
-<<<<<<< HEAD
-=======
-		Console.WriteLine("My Branche (Paule1511)");
-		
->>>>>>> 384ab95e4673e1e349fb785495f21598f1ff857e
+        //this is me
+
+        string m_localText = "Second Hello World";
+
+        Console.WriteLine(m_localText);
     }
 }
