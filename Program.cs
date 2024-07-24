5a72ec0e3975bd2c2435d7cd2fa18ca0e68c37cb
Alexander Holike <alexander.holike@gmail.com>
2024-07-24 12:09:56 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ class Program
         //this is me
 
         string m_localText = "Second Hello World";
+        string m_localString = "This is a additional line";
 
         Console.WriteLine(m_localText);
     }
